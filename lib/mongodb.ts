d83d40432c6ef70812e077292bdf690ad961dd40
SwethaKrishnaAvula <108206328+SwethaKrishnaAvula@users.noreply.github.com>
2025-03-26 09:54:15 -0400

    Create mongodb.ts

---
+++ lib/mongodb.ts
@@ -0,0 +1,9 @@
+import { MongoClient } from "mongodb";
+ if(!process.env.MONGODB_URI){
+    throw new Error("Please add your MONGODB URI to env.local");
+ }
+
+ const client = new MongoClient(process.env.MONGODB_URI);
+ const clientPromise = client.connect();
+
+ export default clientPromise;
